2ecef7034755cb9c0614b082fdb749e439f57a7b
guimaggiorini <guimaggiorini@gmail.com>
2025-11-13 19:21:41 -0300

    Initial Commit

---
+++ skillmate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  skillmate
+//
+//  Created by Gui Maggiorini on 13/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ skillmate/skillmateApp.swift
@@ -0,0 +1,17 @@
+//
+//  skillmateApp.swift
+//  skillmate
+//
+//  Created by Gui Maggiorini on 13/11/25.
+//
+
+import SwiftUI
+
+@main
+struct skillmateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
